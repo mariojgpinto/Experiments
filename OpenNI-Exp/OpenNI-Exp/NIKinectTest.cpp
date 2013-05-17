bd76ee2c8f446f39cd19ed53a133591b988b22cb
Mario Pinto <mariojgpinto@gmail.com>
2013-05-17 16:40:54 +0100

    Test change

--- OpenNI-Exp/OpenNI-Exp/NIKinectTest.cpp
+++ OpenNI-Exp/OpenNI-Exp/NIKinectTest.cpp
@@ -4,8 +4,8 @@
 
 int main_nikinect_test(int argc, char* argv[]){
 	NIKinect* kinect = new NIKinect();
-
 	bool result = false;
+	result = kinect->init("C:\\Dev\\Kinect\\Data\\ONI\\mirror_papers.oni");
 
 	//result = kinect->init_generators();
 
@@ -13,17 +13,30 @@ int main_nikinect_test(int argc, char* argv[]){
 	cv::Mat depth;
 	cv::Mat mask;
 	cv::Mat depthMat8UC1;
-	cv::Mat depth_as_color;
+	cv::Mat3b depth_as_color;
 
+	int _min_bar = 400;
+	int _max_bar = 1500;
+
+	cv::namedWindow("DepthAsColor");
+	cv::createTrackbar("MinDepth", "DepthAsColor", &_min_bar, 5000, NULL);
+	cv::createTrackbar("MaxDepth", "DepthAsColor", &_max_bar, 5000, NULL);
 
 	char c = 0;
 	while((c = cv::waitKey(31)) != 27){
 		if(!kinect->update()) 
 			break;
 
-		//if(kinect->get_color(color)){
-		//	imshow("Color",color);
-		//}
+		kinect->set_min_depth(_min_bar);
+		kinect->set_max_depth(_max_bar);
+		if(kinect->get_color(color)){
+			kinect->get_mask(mask);
+			cv::Mat color2;
+
+			color.copyTo(color2,mask);
+			imshow("mask",mask);	
+			imshow("Color",color2);
+		}
 
 		if(kinect->get_depth(depth)){
 			//depth.convertTo(depthMat8UC1, CV_8UC1,0.05);
@@ -32,7 +45,7 @@ int main_nikinect_test(int argc, char* argv[]){
 			//kinect->get_mask(mask);
 			//imshow("Mask",mask);
 		
-			kinect->get_depth_as_color(depth_as_color,500,2500);
+			kinect->get_depth_as_color(depth_as_color);
 			cv::imshow("DepthAsColor",depth_as_color);
 			//cv::Mat masked_color;
 			//color.copyTo(masked_color,mask);
