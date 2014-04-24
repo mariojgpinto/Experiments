d36d58cbee6aea3621fd7a0804271a182499826e
Mario Pinto <mariojgpinto@gmail.com>
2014-04-24 16:13:15 +0100

    CS2010-Exp
    C# experiences with VS2010

---
+++ CS2010-Exp/CS2010-Exp/Program.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+using System.Windows.Forms;
+using System.IO;
+using System.Collections;
+using System.Diagnostics;
+using System.Drawing.Printing;
+
+
+namespace CS2010_Exp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            print_image();
+        }
+
+        static void load_image(String path)
+        {
+
+        }
+
+        static void print_pdf()
+        {
+            ProcessStartInfo info = new ProcessStartInfo();
+            info.Verb = "print";
+            info.FileName = @"c:\image.jpg";
+            info.CreateNoWindow = true;
+            info.WindowStyle = ProcessWindowStyle.Hidden;
+
+            Process p = new Process();
+            p.StartInfo = info;
+            p.Start();
+
+            p.WaitForInputIdle();
+            System.Threading.Thread.Sleep(15000);
+            if (false == p.CloseMainWindow())
+                p.Kill();
+        }
+
+        public static Image resizeImage(Image imgToResize, Size size)
+        {
+            return (Image)(new Bitmap(imgToResize, size));
+        }
+
+        static private void printDoc_Print(object sender, PrintPageEventArgs ev)
+        {
+            try
+            {
+                Image img = Image.FromFile(@"C:\imageV.jpg");
+
+                float max_width = 775;
+                float max_height = 1105;
+
+                float ratio_img = (float)img.Width / (float)img.Height;
+                float ratio_page = max_width / max_height;
+
+                float w = 0, h = 0;
+                float init_x = 10, init_y = 10;
+
+                if (ratio_img > ratio_page)
+                {
+                    w = max_width;
+                    h = img.Height * max_width / img.Width;
+
+                    float diff = max_height - h;
+                    init_y = diff / 2.0f;
+                }
+                else{
+                    h = max_height;
+                    w = img.Width * max_height / img.Height;
+
+                    float diff = max_width - w;
+                    init_x = diff / 2.0f;
+                }
+
+                Image final = resizeImage(img, new Size((int)w, (int)h));
+
+                PointF pf = new PointF(init_x, init_y);
+                ev.Graphics.DrawImage(final, pf);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        static void print_image()
+        {
+            try
+            {
+                PrintDocument printDoc = new PrintDocument();
+                printDoc.PrintPage += new PrintPageEventHandler(printDoc_Print);
+                printDoc.Print();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+
+    //public partial class Form1 : Form
+    //{
+    //    ImageList list = new ImageList();
+
+    //    public Form1()
+    //    {
+    //        InitializeComponent();
+    //        GetImages();
+    //    }
+
+    //    private void GetImages()
+    //    {
+    //        string path = @"C:\1";
+    //        string[] filter = { ".bmp", ".jpg", ".jpeg", ".png" };
+    //        DirectoryInfo directoryInfo = new DirectoryInfo(path);
+    //        FileInfo[] fileInfo = directoryInfo.GetFiles();
+    //        ArrayList arrayList = new ArrayList();
+
+    //        foreach (FileInfo fi in fileInfo)
+    //            foreach (string s in filter)
+    //                if (s == fi.Extension)
+    //                    arrayList.Add(fi.FullName);
+
+    //        //adding files to image list:
+    //        for (int i = 0; i < arrayList.Count; i++)
+    //        {
+    //            Image img = Image.FromFile(arrayList[i].ToString());
+    //            list.Images.Add(img);
+    //        }
+    //    }
+    //}
+}
---
+++ CS2010-Exp/CS2010-Exp/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("CS2010-Exp")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("CS2010-Exp")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2014")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("807b99ce-e809-4c66-8a17-4375d70bc9d6")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
